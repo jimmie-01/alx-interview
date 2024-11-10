55a5d9639b8e000e625d77a560d8bec6741e39a7
Fakile Razzaq <razaqjimmie91@gmail.com>
2024-11-10 04:00:41 -0800

    N queens puzzle

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,61 +1,44 @@
 #!/usr/bin/python3
-"""Module N-queens"""
-
+""" N queens """
 import sys
 
-def is_safe(board, row, col, N):
-    # Check column on this row
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
-    
-    # Check upper left diagonal
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    
-    # Check upper right diagonal
-    for i, j in zip(range(row, -1, -1), range(col, N)):
-        if board[i][j] == 1:
-            return False
-    
-    return True
 
-def solve_nqueens(N):
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    solutions = []
-    solve_nqueens_util(N, 0, board, solutions)
-    return solutions
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-def solve_nqueens_util(N, row, board, solutions):
-    if row == N:
-        solutions.append([[i, row] for i, cell in enumerate(board[row]) if cell == 1])
-        return
-    
-    for col in range(N):
-        if is_safe(board, row, col, N):
-            board[row][col] = 1
-            solve_nqueens_util(N, row + 1, board, solutions)
-            board[row][col] = 0
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    
-    solutions = solve_nqueens(N)
-    for solution in solutions:
-        print(solution)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
 
-if __name__ == "__main__":
-    main()
+n = int(sys.argv[1])
+
+
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
